0210dc7d807ef2ca8c95bc3bfad4cbb89f4c3fd8
moonblade <moonblade168@gmail.com>
2017-03-31 11:46:05 +0530

    added commands

--- js/script.js
+++ js/script.js
@@ -8,57 +8,128 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-System.register("main", [], function (exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
-    function doCommand() {
-        // var command = $('')
+var constant = {
+    defaultPlayerName: "player",
+    startLocation: "startLocation",
+    commands: {
+        'help': {
+            desc: 'Print this help menu'
+        },
+        'north': {
+            desc: 'Go to the specified direction',
+            alternatives: ['south', 'east', 'west', 'north', 'up', 'down']
+        },
+        'inventory': {
+            desc: 'Print inventory',
+            alternatives: ['inv']
+        },
+        'look': {
+            desc: 'Give description of the room you\'re in',
+            alternatives: ['info']
+        },
+        'open': {
+            desc: 'Try to open the object',
+            alternatives: ['unlock'],
+            extra: '[object]'
+        },
+        'attack': {
+            desc: 'Try to kill the enemy',
+            alternatives: ['kill'],
+            extra: '[enemy]'
+        },
+        'put': {
+            desc: 'Put an object',
+            alternatives: ['place', 'keep', 'fix', 'pour'],
+            extra: '[object]'
+        },
+        'take': {
+            desc: 'Take an object',
+            alternatives: ['pick', 'fill'],
+            extra: '[object]'
+        },
+        'clear': {
+            desc: 'Clear the screen of game text'
+        },
+        'make': {
+            desc: 'Make object if the materials are present',
+            alternatives: ['craft', 'build'],
+            extra: '[object]'
+        },
+        'reset': {
+            desc: 'Start game from beginning again',
+            alternatives: ['redo', 'reboot', 'restart']
+        },
+        'ls': {
+            desc: 'Combination of inventory and look'
+        },
+        'save': {
+            desc: 'Create a checkpoint that can be loaded later',
+            extra: '[tag]'
+        },
+        'load': {
+            desc: 'Load a checkpoint that has been saved',
+            extra: '[tag]'
+        },
     }
-    var constant, Unique, Box, Character, Interactable, player;
-    return {
-        setters: [],
-        execute: function () {
-            constant = {
-                defaultPlayerName: "player",
-                startLocation: "startLocation"
-            };
-            Unique = (function () {
-                function Unique() {
-                }
-                return Unique;
-            }());
-            Box = (function (_super) {
-                __extends(Box, _super);
-                function Box() {
-                    return _super !== null && _super.apply(this, arguments) || this;
-                }
-                return Box;
-            }(Unique));
-            Character = (function (_super) {
-                __extends(Character, _super);
-                function Character(name) {
-                    var _this = _super.call(this) || this;
-                    _this.name = name;
-                    _this.inventory = [];
-                    _this.location = constant.startLocation;
-                    return _this;
-                }
-                Character.prototype.moveTo = function (location) {
-                    console.log("Moving To " + location);
-                    this.location = location;
-                };
-                return Character;
-            }(Unique));
-            Interactable = (function (_super) {
-                __extends(Interactable, _super);
-                function Interactable() {
-                    return _super !== null && _super.apply(this, arguments) || this;
-                }
-                return Interactable;
-            }(Unique));
-            player = new Character(constant.defaultPlayerName);
-            console.log(player);
-            console.log("here");
-        }
+};
+var Command = (function () {
+    function Command() {
+    }
+    Command.prototype.isValid = function () {
     };
-});
+    Command.prototype.get = function () {
+        var str = document.getElementById('command').value;
+        // splits string into an array of words, taking out all whitespace
+        var parts = str.split(/\s+/);
+        // command is the first word in the array, which is removed from the array
+        this.verb = parts.shift();
+        // the rest of the words joined together.  If there are no other words, this will be an empty string
+        this.object = parts.join(' ');
+        if (!this.isValid()) {
+            this.verb = '';
+            this.object = '';
+        }
+        document.getElementById('command').value = "";
+    };
+    return Command;
+}());
+var Unique = (function () {
+    function Unique() {
+    }
+    return Unique;
+}());
+var Box = (function (_super) {
+    __extends(Box, _super);
+    function Box() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Box;
+}(Unique));
+var Character = (function (_super) {
+    __extends(Character, _super);
+    function Character(name) {
+        var _this = _super.call(this) || this;
+        _this.name = name;
+        _this.inventory = [];
+        _this.location = constant.startLocation;
+        return _this;
+    }
+    Character.prototype.moveTo = function (location) {
+        console.log("Moving To " + location);
+        this.location = location;
+    };
+    return Character;
+}(Unique));
+var Interactable = (function (_super) {
+    __extends(Interactable, _super);
+    function Interactable() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Interactable;
+}(Unique));
+function doCommand() {
+    command.get();
+}
+var command = new Command();
+var player = new Character(constant.defaultPlayerName);
+console.log(player);
--- main.ts
+++ main.ts
@@ -1,47 +1,135 @@
-import $ from "jquery";
 var constant = {
-    defaultPlayerName : "player",
-    startLocation:"startLocation"
+    defaultPlayerName: "player",
+    startLocation: "startLocation",
+    commands: {
+        'help': {
+            desc: 'Print this help menu'
+        },
+        'north': {
+            desc: 'Go to the specified direction',
+            alternatives: ['south', 'east', 'west', 'north', 'up', 'down']
+        },
+        'inventory': {
+            desc: 'Print inventory',
+            alternatives: ['inv']
+        },
+        'look': {
+            desc: 'Give description of the room you\'re in',
+            alternatives: ['info']
+        },
+        'open': {
+            desc: 'Try to open the object',
+            alternatives: ['unlock'],
+            extra: '[object]'
+        },
+        'attack': {
+            desc: 'Try to kill the enemy',
+            alternatives: ['kill'],
+            extra: '[enemy]'
+        },
+        'put': {
+            desc: 'Put an object',
+            alternatives: ['place', 'keep', 'fix', 'pour'],
+            extra: '[object]'
+        },
+        'take': {
+            desc: 'Take an object',
+            alternatives: ['pick', 'fill'],
+            extra: '[object]'
+        },
+        'clear': {
+            desc: 'Clear the screen of game text'
+        },
+        'make': {
+            desc: 'Make object if the materials are present',
+            alternatives: ['craft', 'build'],
+            extra: '[object]'
+        },
+        'reset': {
+            desc: 'Start game from beginning again',
+            alternatives: ['redo', 'reboot', 'restart']
+        },
+        'ls': {
+            desc: 'Combination of inventory and look'
+        },
+        'save': {
+            desc: 'Create a checkpoint that can be loaded later',
+            extra: '[tag]'
+        },
+        'load': {
+            desc: 'Load a checkpoint that has been saved',
+            extra: '[tag]'
+        },
+    }
 }
 
-class Unique{
-    public id:String;
-    public name:String;
-    public desc:String;
+class Command {
+
+    verb: String;
+    object: String;
+    constructor() {
+
+    }
+
+    public isValid()
+    {
+
+    }
+
+    public get() {
+        var str = ( < HTMLInputElement > document.getElementById('command')).value;
+        // splits string into an array of words, taking out all whitespace
+        var parts = str.split(/\s+/);
+        // command is the first word in the array, which is removed from the array
+        this.verb = parts.shift();
+        // the rest of the words joined together.  If there are no other words, this will be an empty string
+        this.object = parts.join(' ');
+        if(!this.isValid())
+        {
+            this.verb = '';
+            this.object = '';
+        }
+        (<HTMLInputElement>document.getElementById('command')).value = "";
+    }
 }
-class Box extends Unique{
+
+class Unique {
+    public id: String;
+    public name: String;
+    public desc: String;
+}
+class Box extends Unique {
     public material: String;
     public locked: boolean;
     public unlocksWith: any;
-    public contents: Array<any>;
+    public contents: Array < any > ;
 
 }
 
 class Character extends Unique {
-    inventory: Array<any>;
+    inventory: Array < any > ;
     location: String;
-    constructor(name:String)
-    {
+    constructor(name: String) {
         super();
         this.name = name;
         this.inventory = [];
         this.location = constant.startLocation;
     }
 
-    public moveTo(location){
+    public moveTo(location) {
         console.log("Moving To " + location);
         this.location = location;
     }
 }
 
-class Interactable extends Unique{
+class Interactable extends Unique {}
+
+
+function doCommand() {
+    command.get();
+
 }
 
-function doCommand()
-{
-    // var command = $('')
-}
-
+var command = new Command();
 let player = new Character(constant.defaultPlayerName);
 console.log(player)
-console.log("here");
